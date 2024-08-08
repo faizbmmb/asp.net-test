bdd709d9ab4113fc14b24b0e49fe9f67924645cf
faizbmmb <113884160+faizbmmb@users.noreply.github.com>
2024-08-08 22:39:24 +0800

    Add files via upload

---
+++ Feedback.cs
@@ -0,0 +1,11 @@
+public class Feedback
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+    public byte[] Document { get; set; }
+    public string FeedbackType { get; set; }
+    public string ServiceUsed { get; set; }
+    public string AdditionalComment { get; set; }
+}
